1d6645544049ca079f39e6749548d32c66ed5674
quitLightFE <quitlight101@gmail.com>
2026-02-10 17:57:51 +0500

    api getterlar togirlandi

--- api/index.js
+++ api/index.js
@@ -5,6 +5,6 @@ const api = axios.create({ baseURL: process.env.NEXT_PUBLIC_API_URL });
 export const getDashboardData = () => api.get();
 
 // /Teams teams data
-export const getDataTeams = () => api.get("teams");
+export const getDataTeams = () => api.get("/teams");
 
-export const getMembersData = (id) => api.get(`teams/${id}`);
+export const getMembersData = (id) => api.get(`/teams/${id}`);
