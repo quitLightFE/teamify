e5501ff3a84f71f134140c1a5ebb5dff36b2ce86
quitLightFE <quitlight101@gmail.com>
2026-02-10 17:42:56 +0500

    .env qoshildi

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const api = axios.create({ baseURL: "http://localhost:3000/api" });
+const api = axios.create({ baseURL: process.env.NEXT_PUBLIC_API_URL });
 
 // /Dashboard teams data
 export const getDashboardData = () => api.get();
