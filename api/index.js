d2699a6df3734a5121b05bcd3c30e873fa8ce6d8
quitLightFE <quitlight101@gmail.com>
2026-02-10 18:15:13 +0500

    api slesh added

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 const api = axios.create({ baseURL: process.env.NEXT_PUBLIC_API_URL });
 
 // /Dashboard teams data
-export const getDashboardData = () => api.get();
+export const getDashboardData = () => api.get("/");
 
 // /Teams teams data
 export const getDataTeams = () => api.get("/teams");
